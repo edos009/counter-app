720141b54b5afcd78857858d254615dd225c80e5
edos009 <dostyutyunnik@gmail.com>
2022-09-06 13:00:15 +0300

    Add logic for Counter

--- src/component/Counter/index.jsx
+++ src/component/Counter/index.jsx
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./Counter.module.scss";
 
 const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const handlerMinus = () => {
+    if (count <= 0) {
+      return;
+    }
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  const handlerPlus = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
         <div className={styles.inner}>
           <h1 className={styles.heading}>счетчик:</h1>
-          <p className={styles.text}>0</p>
+          <p className={styles.text}>{count}</p>
           <div className={styles.box_btn}>
-            <button className={styles.btn_minus}>- Минус</button>
-            <button className={styles.btn_plus}>Плюс +</button>
+            <button className={styles.btn_minus} onClick={handlerMinus}>
+              - Минус
+            </button>
+            <button className={styles.btn_plus} onClick={handlerPlus}>
+              Плюс +
+            </button>
           </div>
         </div>
       </div>
